3b6262d4343fa202777d15443f1f59d04783e749
Mohd Faiz <Mfabbasss2004@gmail.com>
2026-03-27 18:49:50 +0530

    Refactor PostCSS config to simplify plugin export

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-// Importing required plugins (optional, just for clarity)
-const tailwindPlugin = {};
-const autoprefixerPlugin = {};
-
-// Creating plugins object step by step
-const pluginsConfig = {
-  tailwindcss: tailwindPlugin,
-  autoprefixer: autoprefixerPlugin
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-// Final export object
-const config = {
-  plugins: pluginsConfig
-};
-
-// Exporting the configuration
-export default config;
